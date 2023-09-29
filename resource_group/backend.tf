37d8d0e707aee970c00457f871716e40cfdf8760
Joseph Beeson <josephbeeson@gmail.com>
2023-09-29 14:41:19 +0200

    resource_group update backend to use sp auth

--- resource_group/backend.tf
+++ resource_group/backend.tf
@@ -1,14 +1,34 @@
 terraform {
   required_version = ">= 0.12.0"
-  required_providers {
-    azurerm = "~> 2.1.0"
-  }
-
   backend "local" {}
 }
 
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "~> 2.1.0"
+  client_id       = var.arm_client_id
+  client_secret   = var.arm_client_secret
+  tenant_id       = var.arm_tenant_id
+  subscription_id = var.arm_subscription_id
   features {}
+}
+
+
+terraform {
+  required_version = ">= 0.12.0"
+  backend "local" {}
+}
+
+variable arm_client_id {
+  type = string
+}
+
+variable arm_client_secret {
+  type = string
+}
+
+variable arm_tenant_id {
+  type = string
+}
+
+variable arm_subscription_id {
+  type = string
 }
